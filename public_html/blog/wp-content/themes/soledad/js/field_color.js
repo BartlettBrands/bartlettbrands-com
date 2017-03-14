4a6d0d63817d8a238ba42cea47305ca49bbceea9
Carolina Pistone <CLP@Carolinas-MacBook-Pro.local>
2017-03-14 16:24:15 -0700

    FTP to Git.

--- public_html/blog/wp-content/themes/soledad/js/field_color.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function ($) {
-    $('input.popup-colorpicker').wpColorPicker({
-    	palettes: ['#bf9f5a', '#f6653c', '#2ac4ea', '#ae81f9', '#FD544F', '#78cd6e']
-    });
-});
--- public_html/blog/wp-content/themes/soledad/searchform.php
+++
@@ -1,5 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <div>
-		<input type="text" class="search-input" placeholder="<?php esc_html_e('Type and hit enter...', 'soledad'); ?>" name="s" id="s" />
-	 </div>
-</form>

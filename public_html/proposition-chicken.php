6260763d31fc947b71f12f1da10fe48812824bb9
Bartlett Brands 4 <bartlettbrands4@Bartletts-MacBook-Pro-4.local>
2017-10-12 14:23:56 -0700

    Updated PropC with new Oakland pics.

--- public_html/proposition-chicken.php
+++ public_html/proposition-chicken.php
@@ -20,15 +20,17 @@ include( $base_dir . '/includes/html-app-header.php' );
     <?php /* Include Projects Intro Content */ echo $projectPageContent_top; ?>
 
     <img src="assets/img/PROJECTS/proposition-chicken/pc-branding-visual-01.jpg" alt="Proposition Chicken | Bartlett Brands">
-    <img src="assets/img/PROJECTS/proposition-chicken/pc-branding-visual-02.jpg" alt="Proposition Chicken | Bartlett Brands">
-    <img src="assets/img/PROJECTS/proposition-chicken/pc-branding-visual-03.jpg" alt="Proposition Chicken | Bartlett Brands">
-    <img src="assets/img/PROJECTS/proposition-chicken/pc-branding-visual-04.jpg" alt="Proposition Chicken | Bartlett Brands">
-    <img src="assets/img/PROJECTS/proposition-chicken/pc-branding-visual-05.jpg" alt="Proposition Chicken | Bartlett Brands">
-    <img src="assets/img/PROJECTS/proposition-chicken/pc-branding-visual-06.jpg" alt="Proposition Chicken | Bartlett Brands">
-    <img src="assets/img/PROJECTS/proposition-chicken/pc-branding-visual-07.jpg" alt="Proposition Chicken | Bartlett Brands">
-    <img src="assets/img/PROJECTS/proposition-chicken/pc-branding-visual-08.jpg" alt="Proposition Chicken | Bartlett Brands">
-    <img src="assets/img/PROJECTS/proposition-chicken/pc-branding-visual-09.jpg" alt="Proposition Chicken | Bartlett Brands">
-    <img src="assets/img/PROJECTS/proposition-chicken/pc-branding-visual-10.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-2.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-3.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-4.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-5.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-6.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-7.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-8.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-9.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-10.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-11.jpg" alt="Proposition Chicken | Bartlett Brands">
+    <img src="assets/img/PROJECTS/proposition-chicken/proposition-chicken-design-branding-12.jpg" alt="Proposition Chicken | Bartlett Brands">
 
     <?php /* Include Projects End Content and Nav */ echo $projectPageContent_bottom; ?>
 

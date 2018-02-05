802933191394dca07fa146fe3360a495a1658cac
Bartlett Brands 4 <bartlettbrands4@Bartletts-MacBook-Pro-4.local>
2018-02-05 10:59:21 -0800

    New images for Biossance.

--- public_html/biossance.php
+++ public_html/biossance.php
@@ -20,22 +20,20 @@ include( $base_dir . '/includes/html-app-header.php' );
     <?php /* Include Projects Intro Content */ echo $projectPageContent_top; ?>
 
     <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-1.jpg" alt="Biossance Branding + Design | Bartlett Brands">
+    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-2.jpg" alt="Biossance Branding + Design | Bartlett Brands">
     <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-3.jpg" alt="Biossance Branding + Design | Bartlett Brands">
-    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-4.jpg" alt="Biossance Branding + Design | Bartlett Brands">
+    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-4.gif" alt="Biossance Branding + Design | Bartlett Brands">
     <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-5.jpg" alt="Biossance Branding + Design | Bartlett Brands">
     <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-6.jpg" alt="Biossance Branding + Design | Bartlett Brands">
     <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-7.jpg" alt="Biossance Branding + Design | Bartlett Brands">
     <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-8.jpg" alt="Biossance Branding + Design | Bartlett Brands">
     <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-9.jpg" alt="Biossance Branding + Design | Bartlett Brands">
-    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-10.gif" alt="Biossance Branding + Design | Bartlett Brands">
+    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-10.jpg" alt="Biossance Branding + Design | Bartlett Brands">
     <iframe src="//player.vimeo.com/video/213582843?byline=0&amp;portrait=0&amp;badge=0" allowfullscreen></iframe>
-    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-11.jpg" alt="Biossance Branding + Design | Bartlett Brands">
+    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-11.gif" alt="Biossance Branding + Design | Bartlett Brands">
     <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-12.jpg" alt="Biossance Branding + Design | Bartlett Brands">
     <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-13.jpg" alt="Biossance Branding + Design | Bartlett Brands">
-    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-14.gif" alt="Biossance Branding + Design | Bartlett Brands">
-    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-15.jpg" alt="Biossance Branding + Design | Bartlett Brands">
-    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-16.jpg" alt="Biossance Branding + Design | Bartlett Brands">
-        <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-2.jpg" alt="Biossance Branding + Design | Bartlett Brands">
+    <img src="assets/img/PROJECTS/biossance/biossance-branding-packaging-14.jpg" alt="Biossance Branding + Design | Bartlett Brands">
 
     <?php /* Include Projects End Content and Nav */ echo $projectPageContent_bottom; ?>
 
